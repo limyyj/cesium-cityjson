42a390405d2b1f19444dcda9064cfcfae143bd17
Huiyao <sunhuiyao108@gmail.com>
2018-03-29 10:05:36 +0800

    update viewer

--- src/app/mobius-cesium/viewer/viewer.component.ts
+++ src/app/mobius-cesium/viewer/viewer.component.ts
@@ -51,41 +51,196 @@ export class ViewerComponent extends DataSubscriber {
       var entities = dataSource.entities.values;
       for (var i = 0; i < entities.length; i++) {
         var entity = entities[i];                               
-        entity.polygon.extrudedHeight = entity.properties.HB_LIMIT;
-        entity.polygon.material = Cesium.Color.WHITE.withAlpha(1);
 		entity.polygon.outline = false;
       }
     });
     viewer.zoomTo(promise);
-	//this.colorByHeight(promise,viewer);
+	this.colorByStatus_Cat(promise,viewer);
+	this.HeightByHB_LIMIT(promise,viewer);
 	this.cesiumviewer=viewer;
 	this.cesiumpromise=promise;
   }
 
-  onChange(deviceValue){
-  	if(deviceValue==="Height"){
-  	  this.colorByHeight(this.cesiumpromise,this.cesiumviewer);
-  	}else if(deviceValue==="Area"){
-
-  	}else if(deviceValue==="Distance"){
-
+  onChangeColor(ColorValue){
+  	if(ColorValue==="Status_Cat"){
+  	  this.colorByStatus_Cat(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="DIST_EWL"){
+	  this.colorByDIST_EWL(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="DIST_FEED"){
+	  this.colorByDIST_FEED(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="DIST_TRUNK"){
+	  this.colorByDIST_TRUNK(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="AVAILABLE"){
+	  this.colorByAVAILABLE(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="AGG_POT"){
+	  this.colorByAGG_POT(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="GPR"){
+	  this.colorByGPR(this.cesiumpromise,this.cesiumviewer);
+  	}else if(ColorValue==="HB_LIMIT"){
+  	  this.colorByHB_LIMIT(this.cesiumpromise,this.cesiumviewer);
   	}
 
   }
 
-  colorByHeight(promise,viewer) {
+  colorByStatus_Cat(promise,viewer) {
   	promise.then(function(dataSource) {
       var entities = dataSource.entities.values;
       for (var i = 0; i < entities.length; i++) {
         var entity = entities[i];
-	    if(entity.properties.HB_LIMIT>=100) entity.polygon.material=Cesium.Color.GREEN.withAlpha(1);
-	    else if(entity.properties.HB_LIMIT>=80) entity.polygon.material=Cesium.Color.RED.withAlpha(1);
-	    else if(entity.properties.HB_LIMIT>=60) entity.polygon.material=Cesium.Color.GOLD.withAlpha(1);
-	    else if(entity.properties.HB_LIMIT>=40) entity.polygon.material=Cesium.Color.HOTPINK.withAlpha(1);
-	    else if(entity.properties.HB_LIMIT>=20) entity.polygon.material=Cesium.Color.LIGHTSLATEGREY.withAlpha(1);
-	    else entity.polygon.material=Cesium.Color.WHITE.withAlpha(1);
+        if(entity.properties.Status_Cat!==undefined){
+		  if(entity.properties.Status_Cat._value==="Available") entity.polygon.material=Cesium.Color.LIGHTCORAL.withAlpha(1);
+		  else if(entity.properties.Status_Cat._value==="Prime") entity.polygon.material=Cesium.Color.RED.withAlpha(1);
+		  else if(entity.properties.Status_Cat._value==="Remnant") entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);
+          else if(entity.properties.Status_Cat._value==="Estate under active master / infra planning") entity.polygon.material=Cesium.Color.CRIMSON.withAlpha(1);
+		  else entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);
+		}else{entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);}
       }
     });
   }
+
+  colorByDIST_FEED(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.DIST_FEED!==undefined){
+	      if(entity.properties.DIST_FEED>=239) entity.polygon.material=Cesium.Color.DARKCYAN .withAlpha(1);
+	      else if(entity.properties.DIST_FEED>=151) entity.polygon.material=Cesium.Color.MEDIUMTURQUOISE.withAlpha(1);
+	      else if(entity.properties.DIST_FEED>=96) entity.polygon.material=Cesium.Color.KHAKI.withAlpha(1);
+	      else if(entity.properties.DIST_FEED>=61) entity.polygon.material=Cesium.Color.GOLD.withAlpha(1);
+	      else entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);
+	  	}else{entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);}
+      }
+    });
+  }
+
+  colorByDIST_TRUNK(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.DIST_FEED!==undefined){
+	      if(entity.properties.DIST_TRUNK>=239) entity.polygon.material=Cesium.Color.DARKCYAN .withAlpha(1);
+	      else if(entity.properties.DIST_TRUNK>=151) entity.polygon.material=Cesium.Color.MEDIUMTURQUOISE.withAlpha(1);
+	      else if(entity.properties.DIST_TRUNK>=96) entity.polygon.material=Cesium.Color.KHAKI.withAlpha(1);
+	      else if(entity.properties.DIST_TRUNK>=61) entity.polygon.material=Cesium.Color.GOLD.withAlpha(1);
+	      else entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);
+	  	}else{entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);}
+      }
+    });
+  }
+
+  colorByDIST_EWL(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.DIST_FEED!==undefined){
+	      if(entity.properties.DIST_EWL>=715) entity.polygon.material=Cesium.Color.DARKCYAN .withAlpha(1);
+	      else if(entity.properties.DIST_EWL>=451) entity.polygon.material=Cesium.Color.MEDIUMTURQUOISE.withAlpha(1);
+	      else if(entity.properties.DIST_EWL>=286) entity.polygon.material=Cesium.Color.KHAKI.withAlpha(1);
+	      else if(entity.properties.DIST_EWL>=181) entity.polygon.material=Cesium.Color.GOLD.withAlpha(1);
+	      else entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);
+	  	}else{entity.polygon.material=Cesium.Color.CORAL.withAlpha(1);}
+      }
+    });
+  }
+
+  colorByAVAILABLE(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.AVAILABLE!==undefined){
+	      if(entity.properties.AVAILABLE._value==="AVAILABLE") entity.polygon.material=Cesium.Color.LIGHTCORAL.withAlpha(1);
+		  else if(entity.properties.AVAILABLE._value==="COMMITTED") entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);
+		}else{entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);}
+      }
+    });
+  }
+
+  colorByAGG_POT(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.AGG_POT!==undefined){
+	      if(entity.properties.AGG_POT>=0.79) entity.polygon.material=Cesium.Color.CORAL .withAlpha(1);
+	      else if(entity.properties.AGG_POT>=0.67) entity.polygon.material=Cesium.Color.GOLD.withAlpha(1);
+	      else if(entity.properties.AGG_POT>=0.56) entity.polygon.material=Cesium.Color.KHAKI.withAlpha(1);
+	      else if(entity.properties.AGG_POT>=0.38) entity.polygon.material=Cesium.Color.MEDIUMTURQUOISE.withAlpha(1);
+	      else entity.polygon.material=Cesium.Color.DARKCYAN.withAlpha(1);
+	  	}else{entity.polygon.material=Cesium.Color.DARKCYAN.withAlpha(1);}
+      }
+    });
+  }
+
+  colorByGPR(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.GPR!==undefined){
+		  if(entity.properties.GPR._value==="1.0"||entity.properties.GPR._value===1.0) entity.polygon.material=Cesium.Color.ORANGERED.withAlpha(1);
+		  else if(entity.properties.GPR._value==="1.4"||entity.properties.GPR._value===1.4) entity.polygon.material=Cesium.Color.DARKORANGE.withAlpha(1);
+		  else if(entity.properties.GPR._value==="2.0"||entity.properties.GPR._value===2.0) entity.polygon.material=Cesium.Color.YELLOW.withAlpha(1);
+		  else if(entity.properties.GPR._value==="2.5"||entity.properties.GPR._value===2.5) entity.polygon.material=Cesium.Color.LIGHTYELLOW.withAlpha(1);
+		  else {entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);}
+		}else{entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);}
+      }
+    });
+  }
+
+  colorByHB_LIMIT(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.HB_LIMIT!==undefined){
+	      if(entity.properties.HB_LIMIT>=93) entity.polygon.material=Cesium.Color.ORANGERED.withAlpha(1);
+	      else if(entity.properties.HB_LIMIT>=86) entity.polygon.material=Cesium.Color.DARKORANGE.withAlpha(1);
+	      else if(entity.properties.HB_LIMIT>=77) entity.polygon.material=Cesium.Color.YELLOW.withAlpha(1);
+	      else if(entity.properties.HB_LIMIT>=63) entity.polygon.material=Cesium.Color.LIGHTYELLOW.withAlpha(1);
+	      else entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);
+	  	}else{entity.polygon.material=Cesium.Color.LIGHTSLATEGRAY.withAlpha(1);}
+      }
+    });
+  }
+
+  onChangeHeight(HeightValue){
+  	if(HeightValue==="HB_LIMIT"){
+  	  this.HeightByHB_LIMIT(this.cesiumpromise,this.cesiumviewer);
+  	}else if(HeightValue==="GPR"){
+	  this.HeightByGPR(this.cesiumpromise,this.cesiumviewer);
+  	}
+  }
+
+  HeightByHB_LIMIT(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.HB_LIMIT!==undefined){
+	      entity.polygon.extrudedHeight = entity.properties.HB_LIMIT*5;
+		}else{entity.polygon.extrudedHeight =0;}
+      }
+    });
+  }
+
+  HeightByGPR(promise,viewer) {
+  	promise.then(function(dataSource) {
+      var entities = dataSource.entities.values;
+      for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        if(entity.properties.GPR!==undefined){
+	      if(entity.properties.GPR._value==="1.0") entity.polygon.extrudedHeight = 10;
+	      else if(entity.properties.GPR._value==="1.4") entity.polygon.extrudedHeight = 15;
+	      else if(entity.properties.GPR._value==="2.0") entity.polygon.extrudedHeight = 60;
+	      else if(entity.properties.GPR._value==="2.5") entity.polygon.extrudedHeight = 90;
+	  	}else{entity.polygon.extrudedHeight = 0;}
+      }
+    });
+  }
+
 
 }
