ca9daeac86e462fd4b02158bc6cf8a54b0969b06
Lim Yan Yee Joie <akilyyj@nus.edu.sg>
2018-11-07 12:08:16 +0800

    Local engineering CRS and update about + examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import {DataService} from "./mobius-cesium/data/data.service";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  public gs_dummy_data: any=undefined;
+  // public gs_dummy_data: any=undefined;
   public mode: string;
-  public rules : any;
+  public rules: any;
 
   constructor(private dataService: DataService) {
     // this.dataService.setGsModel(this.gs_dummy_data);
@@ -17,21 +17,23 @@ export class AppComponent {
 
   public handleFileSelect(evt) {
     const files = evt.target.files; // FileList object
-    evt = null;
     const filetype = files[0].type;
     const fr = new FileReader();
     const self = this;
     fr.onload = function(text) {
       if (filetype === "application/json") {
         const js_data = JSON.parse(text.target["result"]);
-        self.gs_dummy_data = js_data;
+        // self.gs_dummy_data = js_data;
+        self.dataService.setGsModel(js_data);
       } else {
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(text.target["result"],"application/xml");
-        self.gs_dummy_data = xmlDoc;
+        // self.gs_dummy_data = xmlDoc;
+        self.dataService.setGsModel(xmlDoc);
       }
     };
     fr.readAsText(files[0]);
+    evt.target.value = "";
     document.getElementById("overlay").style.display = "block";
     document.getElementById("loader").style.display = "block";
   }
--- src/app/mobius-cesium/setting/about.component.ts
+++ src/app/mobius-cesium/setting/about.component.ts
@@ -9,4 +9,44 @@ import * as chroma from "chroma-js";
   templateUrl: "./about.component.html",
   styleUrls: ["./attributes.component.css"],
 })
-export class AboutComponent { }
+export class AboutComponent { 
+
+	constructor(private dataService: DataService) { }
+
+	public handleExamples(id) {
+		const path = "./assets/examples/" + id;
+    const readfile = new Promise(function(resolve) {
+      let val = "";
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+          val = xhttp.responseText;
+          resolve(val);
+        }
+      };
+      xhttp.open("GET", path, true);
+      xhttp.send();
+    });
+
+    readfile.then((file) => {
+    	const data = this.parseFile(file);
+    	this.dataService.setGsModel(data);
+      document.getElementById("overlay").style.display = "block";
+    	document.getElementById("loader").style.display = "block";
+    });
+  }
+
+  public parseFile(file): any {
+  	const filetype = file.type;
+    let data: any;
+    try {
+      const js_data = JSON.parse(file);
+      data = js_data;
+    } catch {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(file,"application/xml");
+      data = xmlDoc;
+    }
+    return data;
+  }
+}
